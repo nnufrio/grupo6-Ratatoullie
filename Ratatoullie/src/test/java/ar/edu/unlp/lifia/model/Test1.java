942cad0c4eb0dbef278c72e270b647eea88cf7f1
adnan <adnan@DESKTOP-7U20QHD>
2017-06-05 19:15:08 -0300

    Se agregó el test stopFollowing().

--- Ratatoullie/src/test/java/ar/edu/unlp/lifia/model/Test1.java
+++ Ratatoullie/src/test/java/ar/edu/unlp/lifia/model/Test1.java
@@ -27,7 +27,7 @@ public class Test1 {
 	}
 	
 	@Test
-	public void userStopFollowing(){
+	public void userStopFollowing(){//agregado
 		user1.follow(user2);
 		user1.stopFollowing(user2);
 		Assert.assertFalse(user1.getFollowings().contains(user2));
