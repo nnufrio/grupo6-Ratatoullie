84d2ff4fb9905315c044ff5780cd8af56c99342d
adnan <ferrer.adnan@gmail.com>
2017-06-07 04:32:39 -0300

    Added testRecomend()

--- Ratatoullie/src/test/java/ar/edu/unlp/lifia/model/Test1.java
+++ Ratatoullie/src/test/java/ar/edu/unlp/lifia/model/Test1.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import ar.edu.unlp.lifia.model.Location;
 import ar.edu.unlp.lifia.model.User;
 
-
-
 public class Test1 {
 
 	User user1;
@@ -35,5 +33,12 @@ public class Test1 {
 		Assert.assertFalse(user2.getFollowers().contains(user1));
 	}
 	
-	
+	@Test
+	public void testRecommend(){
+		UserResponsible resUser = new UserResponsible("resUser", new Location(2, 3));
+		Restaurant r = new Restaurant("FastFood", 07062017, new Location(2, 3), resUser);
+		Menu m = new Menu(r, "Locro", 07062017);
+		user1.recommend(m, user2, "Bueno", 07062017);
+		Assert.assertFalse(user2.getRecommendations().isEmpty());
+	}
 }
